ec780760ebfd1410dc75d5d303541df9180000b5
Akshaya-vc <akshayav7c@gmail.com>
2021-09-29 01:54:55 +0530

    added Timeline

--- src/components/icons/icons.jsx
+++ src/components/icons/icons.jsx
@@ -1,10 +1,17 @@
-// import React from "react"
+import React from "react"
+import Session1 from "../../assets/svg/sessions-svg/sessions-1.svg";
+import Session2 from "../../assets/svg/sessions-svg/sessions-2.svg";
+import codesprint1 from "../../assets/svg/codesprint-svg/codesprint-right.svg";
+import codesprint2 from "../../assets/svg/codesprint-svg/codesprint-left.svg"
 
-// const icons = () = {
-//     return(
-//         <div>
-//         <img src="" alt="" />
-//         </div>
-        
-//     )
-// }
+import "./icons.css";
+
+const iconslist = [Session1, Session2, codesprint1, codesprint2]
+
+const Icons = (props) => {
+    return(  <img src={iconslist[props.id]} alt="" className={`icons ${props.id==4?"icons-tree":""}`}/>
+       
+    )
+}
+
+export default Icons;
